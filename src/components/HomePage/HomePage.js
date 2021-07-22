1106962232e4d1ba218818ccf999fe227613846d
khan3firoz <khan3firoz@gmail.com>
2021-07-22 19:58:02 +0530

    add server

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,12 +2,12 @@ import React,{useEffect} from 'react';
 import './HomePage.css';
 
 const HomePage = ({ setLoginUser }) => {
-    useEffect(() => {
-        fetch('http://localhost:9002/api').then(response => response.json())
-            .then(data => {
-            console.log(data)
-        })
-    })
+    // useEffect(() => {
+    //     fetch('http://localhost:9002/api').then(response => response.json())
+    //         .then(data => {
+    //         console.log(data)
+    //     })
+    // })
     return (
         <div className="homepage">
             <h1>Hello Homepage</h1>
